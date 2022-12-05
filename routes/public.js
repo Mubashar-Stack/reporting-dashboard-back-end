8dc1647d7e212b733ebe33fa71b82ca9ce22117f
Farzan Khan <farzankhan009@gmail.com>
2022-12-05 17:49:18 +0500

    add auth middleware

--- routes/public.js
+++ routes/public.js
@@ -14,44 +14,44 @@ const final_payable = require("../controllers/final_payable");
 router
   .route("/auth/login")
   .post(validate(authValidation.loginParam), auth.login);
-router.route("/users/:id").get(users.getUserById);
-router.route("/users").get(users.getAllUsers);
-router.route("/user/add").post(users.addUser);
+router.route("/users/:id").get(authorize.authorize,users.getUserById);
+router.route("/users").get(authorize.authorize, users.getAllUsers);
+router.route("/user/add").post(authorize.authorize, users.addUser);
 
-router.route("/user/update/:id").put(users.updateUser);
-router.route("/user/delete/:id").delete(users.deleteUser);
+router.route("/user/update/:id").put(authorize.authorize, users.updateUser);
+router.route("/user/delete/:id").delete(authorize.authorize, users.deleteUser);
 
-router.route("/domains/:id").get(domains.getDomainById);
-router.route("/domains").get(domains.getAllDomains);
-router.route("/domain/add").post(domains.addDomain);
+router.route("/domains/:id").get(authorize.authorize, domains.getDomainById);
+router.route("/domains").get(authorize.authorize, domains.getAllDomains);
+router.route("/domain/add").post(authorize.authorize, domains.addDomain);
 
-router.route("/domain/update/:id").put(domains.updateDomain);
-router.route("/domain/delete/:id").delete(domains.deleteDomain);
+router.route("/domain/update/:id").put(authorize.authorize, domains.updateDomain);
+router.route("/domain/delete/:id").delete(authorize.authorize, domains.deleteDomain);
 
-router.route("/users_domains/:id").get(users_domains.getUserDomainById);
-router.route("/users_domains_by_user_id/:user_id").get(users_domains.getUserDomainByUserId);
-router.route("/users_domains").get(users_domains.getAllUserDomains);
-router.route("/user_domain/add").post(users_domains.addUserDomain);
-router.route("/user_domain/update/:id").put(users_domains.updateUserDomain);
-router.route("/user_domain/delete/:user_id/:domain_id").delete(users_domains.deleteUserDomain);
-
-
-
-router.route("/reports/new").post(reports.addReport);
-router.route("/reports/all").get(reports.getAllFiles);
-router.route("/reports/delete/:id").delete(reports.deleteFile);
-
-router.route("/homeStats").get(reports.getHomeStats);
-router.route("/userHomeStats").get(reports.getUserHomeStats);
-router.route("/homeStatsFixed").get(reports.getHomeStatsFixed);
-router.route("/userHomeStatsFixed").get(reports.getUserHomeStatsFixed);
+router.route("/users_domains/:id").get(authorize.authorize, users_domains.getUserDomainById);
+router.route("/users_domains_by_user_id/:user_id").get(authorize.authorize, users_domains.getUserDomainByUserId);
+router.route("/users_domains").get(authorize.authorize, users_domains.getAllUserDomains);
+router.route("/user_domain/add").post(authorize.authorize, users_domains.addUserDomain);
+router.route("/user_domain/update/:id").put(authorize.authorize, users_domains.updateUserDomain);
+router.route("/user_domain/delete/:user_id/:domain_id").delete(authorize.authorize, users_domains.deleteUserDomain);
 
 
-router.route("/final-payable/new").post(final_payable.addFinalPayable);
-router.route("/monthly-payable").get(final_payable.getMonthlyReport);
-router.route("/user-monthly-payable").get(final_payable.getUserMonthlyReport);
-router.route("/final_payable/all").get(final_payable.getAllFiles);
-router.route("/final_payable/delete/:id").delete(final_payable.deleteFile);
+
+router.route("/reports/new").post(authorize.authorize, reports.addReport);
+router.route("/reports/all").get(authorize.authorize, reports.getAllFiles);
+router.route("/reports/delete/:id").delete(authorize.authorize, reports.deleteFile);
+
+router.route("/homeStats").get(authorize.authorize, reports.getHomeStats);
+router.route("/userHomeStats").get(authorize.authorize, reports.getUserHomeStats);
+router.route("/homeStatsFixed").get(authorize.authorize, reports.getHomeStatsFixed);
+router.route("/userHomeStatsFixed").get(authorize.authorize, reports.getUserHomeStatsFixed);
+
+
+router.route("/final-payable/new").post(authorize.authorize, final_payable.addFinalPayable);
+router.route("/monthly-payable").get(authorize.authorize, final_payable.getMonthlyReport);
+router.route("/user-monthly-payable").get(authorize.authorize, final_payable.getUserMonthlyReport);
+router.route("/final_payable/all").get(authorize.authorize, final_payable.getAllFiles);
+router.route("/final_payable/delete/:id").delete(authorize.authorize, final_payable.deleteFile);
 
 
 
